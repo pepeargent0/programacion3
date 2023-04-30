b802a00af8393e5105433688a96a298f1011f645
pepe <dani_rc_22@hotmail.com>
2023-04-30 03:08:00 -0300

    greedy

--- src/pathfinder/search/gbfs.py
+++ src/pathfinder/search/gbfs.py
@@ -1,83 +1,45 @@
-"""
 from ..models.grid import Grid
 from ..models.frontier import PriorityQueueFrontier
 from ..models.solution import NoSolution, Solution
 from ..models.node import Node
-
-
-class GreedyBestFirstSearch:
-    @staticmethod
-    def search(grid: Grid) -> Solution:
-        # Initialize a node with the initial position
-        node = Node("", grid.start, 0)
-        print(grid.start, grid.end)
-
-        # Initialize the explored dictionary to be empty
-        explored = {}
-        frontier = PriorityQueueFrontier()
-        frontier.add(node, grid.heuristic(node.state, grid.end))
-
-        while True:
-            if frontier.is_empty():
-                return NoSolution(explored)
-            node = frontier.pop()
-            print(node, node.cost)
-            explored[node.state] = True
-            if node.state == grid.end:
-                return Solution(node, explored)
-            neighbours = grid.get_neighbours(node.state)
-            for neighbor_state in neighbours:
-                if neighbor_state not in explored:
-
-                    new_node = Node("", neighbor_state, node.cost + grid.get_cost(neighbor_state))
-                    new_node.parent = node
-                    new_node.action = neighbor_state
-                    frontier.add(new_node, grid.heuristic(neighbor_state, grid.end))
-                    print('frontier', frontier)
-                    print('node', new_node)
-        return NoSolution(explored)
-"""
-from ..models.grid import Grid
-from ..models.frontier import PriorityQueueFrontier
-from ..models.solution import NoSolution, Solution
-from ..models.node import Node
+from typing import Dict, Tuple
 
 
 class GreedyBestFirstSearch:
     @staticmethod
     def search(grid: Grid) -> Solution:
         """
-            Find path between two points in a grid using Depth First Search
-                Args:
-                    grid (Grid): Grid of points
+        Find the shortest path between two points in a grid using Greedy Best First Search algorithm.
 
-                Returns:
-                    Solution: Solution found
+        Args:
+            grid (Grid): A grid containing the start and end positions.
+
+        Returns:
+            Solution: The solution containing the path and the explored nodes.
         """
         # Initialize a node with the initial position
         node = Node("", grid.start, 0)
-        node.f_cost = node.cost + grid.heuristic(node.state, grid.end)
-        # Initialize the explored dictionary to be empty
-        explored = {}
+        node.f_cost = node.cost + int(grid.heuristic(node.state, grid.end))
+        # Initialize the explored set to be empty
+        explored: Dict[Tuple[int, int], bool] = {}
+        # Initialize the frontier with the starting node
         frontier = PriorityQueueFrontier()
-        # frontier.add(node, grid.heuristic(node.state, grid.end))
         frontier.add(node)
-        print(frontier)
-        while True:
-            if frontier.is_empty():
-                return NoSolution(explored)
+
+        while not frontier.is_empty():
             node = frontier.pop()
-            explored[node.state] = True
             if node.state == grid.end:
                 return Solution(node, explored)
+            if node.state in explored:
+                continue
+            explored[node.state] = True
             neighbours = grid.get_neighbours(node.state)
-            for neighbor_state in neighbours:
-                if neighbor_state not in explored:
-                    new_state = neighbours[neighbor_state]
+            for neighbour_state in neighbours:
+                new_state = neighbours[neighbour_state]
+                if neighbour_state not in explored:
                     new_node = Node("", new_state, node.cost + grid.get_cost(new_state))
                     new_node.parent = node
-                    new_node.action = neighbor_state
-                    new_node.f_cost = node.cost + grid.get_cost(new_state) + grid.heuristic(new_state, grid.end)
+                    new_node.action = neighbour_state
+                    new_node.f_cost = new_node.cost + grid.heuristic(new_state, grid.end)
                     frontier.add(new_node)
-
         return NoSolution(explored)
