c5afb8cc206e3c111d9d8600d1b717bb850e30fb
pepe <dani_rc_22@hotmail.com>
2023-04-30 02:13:40 -0300

    feat: search ucs

--- src/pathfinder/search/ucs.py
+++ src/pathfinder/search/ucs.py
@@ -1,5 +1,6 @@
-from ..models.grid import Grid
 from ..models.frontier import PriorityQueueFrontier
+from typing import Dict, Tuple
+from ..models.grid import Grid
 from ..models.solution import NoSolution, Solution
 from ..models.node import Node
 
@@ -7,37 +8,24 @@ from ..models.node import Node
 class UniformCostSearch:
     @staticmethod
     def search(grid: Grid) -> Solution:
-        """Find path between two points in a grid using Uniform Cost Search
-
-        Args:
-            grid (Grid): Grid of points
-
-        Returns:
-            Solution: Solution found
-        """
         # Initialize a node with the initial position
-        node = Node("", grid.start, 0)
-        explored = {}
+        start_node = Node("", grid.start, 0)
+        explored: Dict[Tuple[int, int], bool] = {}
         frontier = PriorityQueueFrontier()
-        frontier.add(node)
-        while True:
-            if frontier.is_empty():
-                return NoSolution(explored)
+        frontier.add(start_node)
+        while not frontier.is_empty():
             node = frontier.pop()
+            if node.state in explored:
+                continue
             explored[node.state] = True
-
             if node.state == grid.end:
                 return Solution(node, explored)
-
-            if node.state not in explored or node.cost < explored[node.state]:
-                explored[node.state] = node.cost
-                neighbours = grid.get_neighbours(node.state)
-                for neighbour_state in neighbours:
-                    new_state = neighbours[neighbour_state]
-                    new_cost = node.cost + grid.get_cost(new_state)
-                    new_node = Node(neighbour_state, new_state, new_cost, node)
+            neighbours = grid.get_neighbours(node.state)
+            for neighbor_state in neighbours:
+                new_state = neighbours[neighbor_state]
+                if neighbours[neighbor_state] not in explored:
+                    new_node = Node("", new_state, node.cost + grid.get_cost(new_state))
+                    new_node.parent = node
+                    new_node.action = neighbor_state
                     frontier.add(new_node)
-                    print(neighbour_state)
-                    print(node.cost, grid.get_cost(new_state))
-
         return NoSolution(explored)
